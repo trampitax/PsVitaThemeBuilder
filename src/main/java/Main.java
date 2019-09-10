fc370cca84c86f9e832078ada46813ff215ee342
trampitax <trampitaxonfire@gmail.com>
2019-09-10 18:38:59 +0200

    Add: Dinamically show and hide pages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,92 +14,195 @@ public class Main {
     private JTextField themeTitleTextField;
     private JTextField authorNameTextField;
     private JButton buildButton;
-    private JTextField textField1;
+    private JTextField lockScreenPath;
     private JButton searchButtonLockScreen;
     private JButton searchButton01Background;
     private JPanel contentPanel;
+    private JButton searchButton02Background;
+    private JButton searchButton03Background;
+    private JButton searchButton04Background;
+    private JButton searchButton05Background;
+    private JButton searchButton06Background;
+    private JButton searchButton07Background;
+    private JButton searchButton08Background;
+    private JButton searchButton09Background;
+    private JButton searchButton10Background;
+    private JTextField page1Path;
+    private JLabel infoLabel;
+    private JPanel page2Panel;
+    private JPanel page3Panel;
+    private JPanel page4Panel;
+    private JPanel page5Panel;
+    private JPanel page6Panel;
+    private JPanel page7Panel;
+    private JPanel page8Panel;
+    private JPanel page9Panel;
+    private JPanel page10Panel;
+    private JButton plusButton;
+    private JButton lessButton;
 
     private ArrayList<String> imagesPaths;
 
     private Main() {
         imagesPaths = new ArrayList<String>();
+        lessButton.setEnabled(false);
 
-       /* String path = "xd.png";
-        try {
-            BufferedImage picture = ImageFormatter.convertRGBAToIndexed(path);
-            System.out.println(picture);
+        final JPanel [] extraPanels = {
+                null,
+                null,
+                page2Panel,
+                page3Panel,
+                page4Panel,
+                page5Panel,
+                page6Panel,
+                page7Panel,
+                page8Panel,
+                page9Panel,
+                page10Panel
+        };
 
-            ImageIO.write(picture, "png", new File("dest.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+        for (JPanel panel : extraPanels) {
+            if(panel != null) {
+                panel.setVisible(false);
+            }
+        }
 
-       ActionListener srchAction = new ActionListener() {
-           public void actionPerformed(ActionEvent e) {
-               JButton xd = (JButton) e.getSource();
-               JTextField sap = (JTextField) xd.getParent().getComponent(0);
-               System.out.println();
+        getTextFieldsPaths();
 
-               JFileChooser chooser = new JFileChooser();
-               FileNameExtensionFilter filter = new FileNameExtensionFilter("png files (*.png)", "png");
-               chooser.setFileFilter(filter);
-               int returnVal = chooser.showOpenDialog(panelMain);
-               if(returnVal == JFileChooser.APPROVE_OPTION) {
-                   System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
-                   String filePath = chooser.getSelectedFile().getAbsolutePath();
-                   sap.setText(filePath);
-                   imagesPaths.add(filePath);
-               }
+        ActionListener srchAction = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JButton button = (JButton) e.getSource();
+                JTextField textField = (JTextField) button.getParent().getComponent(0);
+                System.out.println();
 
-               /*
-
-               /https://stackoverflow.com/questions/18774652/how-to-use-jfilechooser-to-find-a-file-location
                 JFileChooser chooser = new JFileChooser();
-                FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos gpx (*.gpx.xml)", "xml");
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("png files (*.png)", "png");
                 chooser.setFileFilter(filter);
                 int returnVal = chooser.showOpenDialog(panelMain);
-                if(returnVal == JFileChooser.APPROVE_OPTION) {
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
                     System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
+                    String filePath = chooser.getSelectedFile().getAbsolutePath();
+                    textField.setText(filePath);
                 }
-                txtRuta.setText(chooser.getSelectedFile().getName());
-                mapa = chooser.getSelectedFile();
+            }
+        };
 
-                */
+        ActionListener viewPanel = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton button = (JButton) e.getSource();
+                if(button.getText().equalsIgnoreCase("+")) {
+                    for(int x = 3; x < 12; x++) {
+                        JPanel childPanel = (JPanel) contentPanel.getAccessibleContext().getAccessibleChild(x);
+                        int position = Integer.parseInt(childPanel.getToolTipText().split(" ")[1]);
+                        JPanel selectedPanel = extraPanels[position];
+                        if(!selectedPanel.isVisible()) {
+                            selectedPanel.setVisible(true);
 
+                            if(selectedPanel.getToolTipText().split(" ")[1].equalsIgnoreCase("10")) {
+                                plusButton.setEnabled(false);
+                            }
+                            lessButton.setEnabled(true);
+                            break;
+                        }
+                    }
+                } else {
+                    for(int x = 11; x > 2; x--) {
+                        JPanel childPanel = (JPanel) contentPanel.getAccessibleContext().getAccessibleChild(x);
+                        int position = Integer.parseInt(childPanel.getToolTipText().split(" ")[1]);
+                        JPanel selectedPanel = extraPanels[position];
+                        if(selectedPanel.isVisible()) {
+                            selectedPanel.setVisible(false);
+                            JTextField textField = (JTextField) selectedPanel.getAccessibleContext().getAccessibleChild(0);
+                            textField.setText("");
 
-           }
-       };
+                            if(selectedPanel.getToolTipText().split(" ")[1].equalsIgnoreCase("2")) {
+                                lessButton.setEnabled(false);
+                            }
+                            plusButton.setEnabled(true);
+                            break;
+                        }
+                    }
+                }
+            }
+        };
 
-       searchButtonLockScreen.addActionListener(srchAction);
-       searchButton01Background.addActionListener(srchAction);
+        plusButton.addActionListener(viewPanel);
+        lessButton.addActionListener(viewPanel);
+
+        searchButtonLockScreen.addActionListener(srchAction);
+        searchButton01Background.addActionListener(srchAction);
+        searchButton02Background.addActionListener(srchAction);
+        searchButton03Background.addActionListener(srchAction);
+        searchButton04Background.addActionListener(srchAction);
+        searchButton05Background.addActionListener(srchAction);
+        searchButton06Background.addActionListener(srchAction);
+        searchButton07Background.addActionListener(srchAction);
+        searchButton08Background.addActionListener(srchAction);
+        searchButton09Background.addActionListener(srchAction);
+        searchButton10Background.addActionListener(srchAction);
 
         buildButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Build button pressed");
-                ArrayList<BufferedImage>  readyImages = new ArrayList<BufferedImage>();
 
-                for(String path : imagesPaths) {
-                    readyImages.add(ImageFormatter.convertRGBAToIndexed(path));
-                }
-
-                int number = 1;
-                String finalNumber = "";
-
-                for(BufferedImage image : readyImages) {
-                    try {
-                        if(number < 10) {
-                           finalNumber = "0" + number;
-                        } else {
-                            finalNumber = "" + number;
-                        }
-                        ImageIO.write(image, "png", new File("outImages/bg_" + finalNumber + ".png"));
-                        number++;
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
+                if(minimumRequirements()) {
+                    infoLabel.setText("Building your theme...");
+                    ArrayList<BufferedImage> readyImages = new ArrayList<BufferedImage>();
+                    for (String path : imagesPaths) {
+                        readyImages.add(ImageFormatter.convertRGBAToIndexed(path));
                     }
+
+                    int number = 1;
+                    String finalNumber = "";
+
+                    for (BufferedImage image : readyImages) {
+                        try {
+                            if (number < 10) {
+                                finalNumber = "0" + number;
+                            } else {
+                                finalNumber = "" + number;
+                            }
+                            ImageIO.write(image, "png", new File("outImages/bg_" + finalNumber + ".png"));
+                            number++;
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                    infoLabel.setText("Finished sucessfully!");
+                } else {
+                    infoLabel.setText("<html><font color='red'>Required fields are empty!</font></html>");
                 }
             }
         });
+    }
+
+    private void getTextFieldsPaths() {
+        for (int x = 1; x < 12; x++) {
+            JPanel thePanel = (JPanel) contentPanel.getAccessibleContext().getAccessibleChild(x);
+            if (thePanel.isVisible()) {
+                JTextField pathTextField = (JTextField) thePanel.getAccessibleContext().getAccessibleChild(0);
+                if (!pathTextField.getText().isEmpty()) {
+                    imagesPaths.add(pathTextField.getText());
+                }
+            }
+        }
+    }
+
+    private boolean minimumRequirements() {
+        boolean result = true;
+
+        if(themeTitleTextField.getText().trim().isEmpty()) {
+            result = false;
+        } else if(authorNameTextField.getText().trim().isEmpty()) {
+            result = false;
+        } else if(lockScreenPath.getText().trim().isEmpty()) {
+            result = false;
+        } else if(page1Path.getText().trim().isEmpty()) {
+            result = false;
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
@@ -110,11 +213,12 @@ public class Main {
             e.printStackTrace();
         }
 
+        // Frame creation
         JFrame frame = new JFrame("PsVita Theme Builder");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(new Main().panelMain);
-//        frame.setSize(450, 300);
-        frame.pack();
+        frame.setSize(450, 300);
+//        frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
