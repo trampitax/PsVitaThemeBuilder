7ed9c65e4246de67c6844b8f0a8ea5a46f229646
trampitax <trampitaxonfire@gmail.com>
2019-09-14 00:21:28 +0200

    XML generation almost finished

--- src/main/java/XML.java
+++ src/main/java/XML.java
@@ -1,3 +1,4 @@
+import org.jdom.Comment;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.output.Format;
@@ -10,14 +11,23 @@ import java.util.ArrayList;
 public class XML {
 
     public static File generateXmlFile() {
-
         ArrayList<String> files = new ArrayList<String>();
-        File directory = new File(Main.OUTPUT + Main.themeTitle);
+        int bgCounter = 0;
+//        File directory = new File(Main.OUTPUT + Main.themeTitle);
 //        File directory = new File("C:\\Users\\Roberto\\IdeaProjects\\PsVitaThemeBuilder\\target\\Themes\\Patata");
+        File directory = new File("/home/roberto/IdeaProjects/PsVitaThemeBuilder/target/Themes/Patata");
         for(File file : directory.listFiles()) {
             files.add(file.getName());
+            if(file.getName().startsWith("bg_")) {
+                bgCounter++;
+            }
             System.out.println("File: " + file.getName());
         }
+
+
+        System.out.println("\nbgCounter TOTAL: "+bgCounter);
+        bgCounter = bgCounter / 2;
+        System.out.println("bgCounter HALF: "+bgCounter);
 
         System.out.println("<---------->");
 
@@ -32,13 +42,86 @@ public class XML {
         Element infomationProperty = new Element("InfomationProperty");
         Element startScreenProperty = new Element("StartScreenProperty");
 
-        // InfomationBarProperty //TODO
+        // HomeProperty
+        Element m_bgParam = new Element("m_bgParam");
 
+        for(int x = 1; x <= bgCounter; x++) {
+            if (x != 10) {
+                String number = "0" + x;
+                m_bgParam.addContent(new Element("BackgroundParam").addContent(new Element("m_imageFilePath").addContent("bg_" + number + ".png")).addContent(new Element("m_thumbnailFilePath").addContent("bg_" + number + "t.png")));
+            } else {
+                m_bgParam.addContent(new Element("BackgroundParam").addContent(new Element("m_imageFilePath").addContent("bg_10.png")).addContent(new Element("m_thumbnailFilePath").addContent("bg_10t.png")));
+            }
+        }
+
+        homeProperty.addContent(m_bgParam);
+
+        if (files.contains("BGM.at9")) {
+           homeProperty.addContent(new Element("m_bgmFilePath").addContent("BGM.at9"));
+        }
+
+        String iconTag = "m_iconFilePath";
+        if(files.contains("icon_calendar.png")) {
+            homeProperty.addContent(new Element("m_calendar").addContent(new Element(iconTag).addContent("icon_calendar.png")));
+        }
+        if(files.contains("icon_cma.png")) {
+            homeProperty.addContent(new Element("m_hostCollabo").addContent(new Element(iconTag).addContent("icon_cma.png")));
+        }
+        if(files.contains("icon_friends.png")) {
+            homeProperty.addContent(new Element("m_friend").addContent(new Element(iconTag).addContent("icon_friends.png")));
+        }
+        if(files.contains("icon_mail.png")) {
+            homeProperty.addContent(new Element("m_email").addContent(new Element(iconTag).addContent("icon_mail.png")));
+        }
+        if(files.contains("icon_messages.png")) {
+            homeProperty.addContent(new Element("m_message").addContent(new Element(iconTag).addContent("icon_messages.png")));
+        }
+        if(files.contains("icon_music.png")) {
+            homeProperty.addContent(new Element("m_music").addContent(new Element(iconTag).addContent("icon_music.png")));
+        }
+        if(files.contains("icon_near.png")) {
+            homeProperty.addContent(new Element("m_near").addContent(new Element(iconTag).addContent("icon_near.png")));
+        }
+        if(files.contains("icon_parental.png")) {
+            homeProperty.addContent(new Element("m_parental").addContent(new Element(iconTag).addContent("icon_parental.png")));
+        }
+        if(files.contains("icon_party.png")) {
+            homeProperty.addContent(new Element("m_party").addContent(new Element(iconTag).addContent("icon_party.png")));
+        }
+        if(files.contains("icon_photos.png")) {
+            homeProperty.addContent(new Element("m_camera").addContent(new Element(iconTag).addContent("icon_photos.png")));
+        }
+        if(files.contains("icon_power.png")) {
+            homeProperty.addContent(new Element("m_power").addContent(new Element(iconTag).addContent("icon_power.png")));
+        }
+        if(files.contains("icon_ps3link.png")) {
+            homeProperty.addContent(new Element("m_ps3Link").addContent(new Element(iconTag).addContent("icon_ps3link.png")));
+        }
+        if(files.contains("icon_ps4link.png")) {
+            homeProperty.addContent(new Element("m_ps4Link").addContent(new Element(iconTag).addContent("icon_ps4link.png")));
+        }
+        if(files.contains("icon_settings.png")) {
+            homeProperty.addContent(new Element("m_settings").addContent(new Element(iconTag).addContent("icon_settings.png")));
+        }
+        if(files.contains("icon_trophies.png")) {
+            homeProperty.addContent(new Element("m_trophy").addContent(new Element(iconTag).addContent("icon_trophies.png")));
+        }
+        if(files.contains("icon_videos.png")) {
+            homeProperty.addContent(new Element("m_video").addContent(new Element(iconTag).addContent("icon_videos.png")));
+        }
+        if(files.contains("icon_web.png")) {
+            homeProperty.addContent(new Element("m_browser").addContent(new Element(iconTag).addContent("icon_web.png")));
+        }
+
+
+        // InfomationBarProperty //TODO INCLUDE IN UI
+        infomationBarProperty.addContent(new Element("m_barColor").addContent("ff8ec7e9"));
+        infomationBarProperty.addContent(new Element("m_indicatorColor").addContent("ff000000"));
 
 
         // InfomationProperty
-        infomationProperty.addContent(new Element("m_provider").addContent(new Element("m_default").addContent(Main.themeAuthor)));
         infomationProperty.addContent(new Element("m_contentVer").addContent("1.00"));
+        infomationProperty.addContent(new Element("m_provider").addContent(new Element("m_default").addContent(Main.themeAuthor)));
         if(files.contains("preview_page.png")) {
             infomationProperty.addContent(new Element("m_homePreviewFilePath").addContent("preview_page.png"));
         }
@@ -49,18 +132,51 @@ public class XML {
             infomationProperty.addContent(new Element("m_packageImageFilePath").addContent("preview_thumbnail.png"));
         }
 
+        Element titleElement = new Element("m_title");
+        titleElement.addContent(new Element("m_default").addContent(Main.themeTitle));
+        Element m_paramTitles = new Element("m_param");
+        String [] languages = {
+                "m_da",
+                "m_de",
+                "m_es",
+                "m_fi",
+                "m_fr",
+                "m_it",
+                "m_nl",
+                "m_no",
+                "m_pl",
+                "m_pt",
+                "m_ru",
+                "m_sv"
+        };
+
+        for(String language : languages) {
+            m_paramTitles.addContent(new Element(language).addContent(Main.themeTitle));
+        }
+
+        titleElement.addContent(m_paramTitles);
+        infomationProperty.addContent(titleElement);
+
+
+        // StartScreenProperty //TODO add color options to UI
+        startScreenProperty.addContent(new Element("m_filePath").addContent("start_bg.png"));
+        startScreenProperty.addContent(new Element("m_dateColor").addContent("ffe4cfb1"));
+        startScreenProperty.addContent(new Element("m_dateLayout").addContent("2"));
+        startScreenProperty.addContent(new Element("m_notifyBgColor").addContent("1fffffff"));
+        startScreenProperty.addContent(new Element("m_notifyBorderColor").addContent("ffcccccc"));
+        startScreenProperty.addContent(new Element("m_notifyFontColor").addContent("ffffffff"));
 
 
         theme.addContent(homeProperty);
         theme.addContent(infomationBarProperty);
         theme.addContent(infomationProperty);
         theme.addContent(startScreenProperty);
+        theme.addContent(new Comment("Template created by trampitax"));
+        theme.addContent(new Comment("https://github.com/trampitax"));
         doc.setRootElement(theme);
 
 
-
         //Create the XML
-
         try {
             XMLOutputter outter=new XMLOutputter();
             outter.setFormat(Format.getPrettyFormat());
